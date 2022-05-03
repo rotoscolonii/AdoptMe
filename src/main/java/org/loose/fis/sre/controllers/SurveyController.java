4fb23e360010230660d88263a6251eb7f399b7e9
rotoscolonii <rotoscolonii@yahoo.com>
2022-05-03 14:18:49 +0300

    Survey

---
+++ src/main/java/org/loose/fis/sre/controllers/SurveyController.java
@@ -0,0 +1,20 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.loose.fis.sre.exceptions.InvalidCredentials;
+import org.loose.fis.sre.services.UserService;
+
+import java.io.IOException;
+
+public class SurveyController {
+
+}
